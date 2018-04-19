50d9058357a68bbeb7a37d6ea99318c4a52677f7
shane acton <shaneacton745.sa@gmail.com>
2018-04-19 13:31:14 +0200

    changed load/save to use the newly added iterator class

--- Image.cpp
+++ Image.cpp
@@ -18,19 +18,31 @@ ACTSHA001::Image::Image() {
 }
 
 ACTSHA001::Image& ACTSHA001::Image::operator+(const ACTSHA001::Image &rhs) {
-    if(height==rhs.height && width==rhs.width){
+    Image im;
+    im.width = width;
+    im.height = height;
+    if(checkCompatible(*this,rhs)){
+        for(int row=0;row<height;row++){
+            im.image.push_back(new unsigned char[width]);
+            for(int col=0;col,width;col++){
+                int num = clamp((*this).image[row][col] + rhs.image[row][col]);
+                cout<<"summing values " << (int)((*this).image[row][col]) << " + " << (int)(rhs.image[row][col]) << " = " << num << endl;
+                im.image[row][col] = (unsigned char)num;
+            }
+        }
+    }
+}
 
-    for(int row=0;row<height;row++){
-        image.push_back(new unsigned char[width]);
-    }}
+unsigned char ACTSHA001::Image::clamp(const int &i) {
+    return (unsigned char)min(255,max(0,i));
 }
 
 bool ACTSHA001::Image::checkCompatible(const ACTSHA001::Image &lhs, const ACTSHA001::Image &rhs) {
     if(height==rhs.height && width==rhs.width){
-
-
+        return true;
     }else{
-        cout<<"images are not the same size";
+        cout<<"images are not the same dimension"<<endl;
+        return false;
     }
 }
 
@@ -52,17 +64,19 @@ void ACTSHA001::Image::loadImage(std::string fileName){
     height = stoi(tokens[0]);
 
     cout<<"width: " <<width << "  height: " << height << endl;
-    //file.close();
-    //image.reserve(height);
+    data = unique_ptr<unsigned char[]>(new unsigned char[width*height]);
 
     getline(file,line);
+
+    iterator it = begin();
     for(int row=0;row<height;row++){
         image.push_back(new unsigned char[width]);
-    }
-
-    for(int row=0;row<height;row++){
         file.read((char*)image[row],width);
         //cout<< "finished "<< (row+1) << endl;
+        for(int col=0;col++;col<width){
+            *it = image[row][col];
+            ++it;
+        }
     }
 
     file.close();
@@ -75,21 +89,45 @@ void ACTSHA001::Image::saveImage(std::string fileName) {
         cout<<"no image loaded to this object"<<endl;
         return;
     }
-    //out.write("P5\n",3);
-    //out.write("#\n",2);
-    //string dims = to_string(height) + " " + to_string(width) ;
-    //cout<<"'"<<dims<<"'"<<endl;
-    //out.write("dims\n",dims.size()+1);
-    //out.write("255\n",4);
+    /**
+    out.write("P5\n",3);
+    out.write("#\n",2);
+    string dims = to_string(height) + " " + to_string(width) ;
+    cout<<"'"<<dims<<"'"<<endl;
+    out.write("dims\n",dims.size()+1);
+    out.write("255\n",4);
+*/
 
 
-
-
+    iterator it = begin();
+    for(auto it = begin(); it!= end();++it){
+        out.put(*it);
+        ++it;
+    }
+/**
     for(int row=0;row<height;row++){
         out.write((char*)image[row],width);
         //cout<< "finished "<< (row+1) << endl;
     }
-
+*/
     out.close();
 }
 
+ACTSHA001::Image::iterator& ACTSHA001::Image::iterator::operator++() {
+    ptr+= sizeof(char);
+    return *this;
+}
+ACTSHA001::Image::iterator& ACTSHA001::Image::iterator::operator--() {
+    ptr-= sizeof(char);
+    return *this;
+}
+ACTSHA001::Image::iterator& ACTSHA001::Image::iterator::operator=(const iterator & rhs) {
+    ptr=rhs.ptr;
+    return *this;
+}
+unsigned char & ACTSHA001::Image::iterator::operator*() {
+    return *ptr;
+}
+bool ACTSHA001::Image::iterator::operator!=(const iterator & rhs) {
+    return ptr!=rhs.ptr;
+}
--- Image.h
+++ Image.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <vector>
+#include <memory>
 
 
 namespace ACTSHA001 {
@@ -15,15 +16,19 @@ namespace ACTSHA001 {
     private:
         //unsigned char** image;
         std::vector<unsigned char*> image;
+        std::unique_ptr<unsigned char[]> data;
+
         int width;
         int height;
 
         bool checkCompatible(const Image & lhs , const Image & rhs );
-
+        unsigned char clamp(const int & i);
 
 
 
     public:
+
+
         void loadImage(std::string file);
         void saveImage(std::string file);
 
@@ -37,6 +42,30 @@ namespace ACTSHA001 {
 
 
         Image();//default constructor
+
+        class iterator
+        {
+        private:
+            friend class Image;
+            unsigned char *ptr;
+// construct only via Image class (begin/end)
+            iterator(unsigned char *p) : ptr(p) {}
+        public://copy construct is public
+            iterator( const iterator & rhs) : ptr(rhs.ptr) {}
+// define overloaded ops: *, ++, --, =
+            iterator & operator=(const iterator & rhs);
+            bool operator!=(const iterator & rhs);
+
+            iterator & operator++();
+            unsigned char  & operator*();
+            iterator & operator--();
+
+
+// other methods for iterator
+        };
+
+        iterator begin(void) { return iterator(data.get());}
+        iterator end(void) { return iterator(&data.get()[width*height]);}
 
     };
 }
